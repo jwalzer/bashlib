cfa7162ac117fcedbd7c726265544174208d30ab
Jan Walzer <jw@1und1.de>
2016-02-18 16:31:11 +0100

    handling Include - Fail when not possible

--- lib/init.bash
+++ lib/init.bash
@@ -1,17 +1,25 @@
 export LC_ALL='C'
 
+LIBDIR="/usr/local/lib/bashlib"
+
 __TOKEN__="$$"
 [ "$INCLUDED_INIT" == "$__TOKEN__" ] && return
 RUNTOKEN="$__TOKEN__"
 INCLUDED_INIT="$RUNTOKEN"
 
-LIBDIR="/usr/local/lib/bashlib"
-
 INCLUDE() {
-	[ -r "$LIBDIR/${1}.bash" ] && . "$LIBDIR/${1}.bash"
+	if [ -r "$LIBDIR/${1}.bash" ] 
+		then
+		 . "$LIBDIR/${1}.bash" 
+		 [[ "$?" -eq 0 ]] || echo "Major Failure, loading $1"
+		 [[ "$?" -eq 0 ]] || exit 1
+		else
+			echo "can't find $1 or not readable"
+			exit 1
+		fi
 }
 
-for lib in functions logging
+for lib in config functions logging
 	do
 		INCLUDE $lib
 	done
