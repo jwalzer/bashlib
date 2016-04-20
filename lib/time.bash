3815363d8c55566e855bd20272b0d396aa0e487c
Jan Walzer <github_w@lzer.net>
2016-04-20 11:57:18 +0200

    implemented functions: 	DIFF_DATETIMESTR - diff two dates in TIMESTR-format 	CONV_SEC2TIME - converts seconds to readable time

--- lib/time.bash
+++ lib/time.bash
@@ -14,6 +14,69 @@ GET_DATETIMESTR() {
 	date -u +%Y%m%d%H%M%S
 }
 
+DIFF_DATETIMESTR() {
+	DEBUG "$@"
+	ASSERT_ISINTEGER "$2" || FAIL 99
+
+	D1="$(CONV_DTSTR2DATE $1)"
+	D2="$(CONV_DTSTR2DATE $2)"
+	D1sec=$(date -d "$D1" +%s)
+	D2sec=$(date -d "$D2" +%s)
+
+	DIFF="$(( D1sec - D2sec ))"
+
+	DEBUG "D1 ($D1) - D2 ($D2) = D1sec ($D1sec) - D2sec ($D2sec) = DIFF ($DIFF) = $DIFF"
+
+	echo "$DIFF"
+}
+
+CONV_SEC2TIME() {
+	DEBUG "$@"
+	ASSERT_ISINTEGER "$1" || FAIL 99
+
+	SEC="$1"
+	SGN=""
+	RET=""
+
+	if [[ "$SEC" -lt 0 ]]
+		then
+			SGN="-"
+			SEC="$(( SEC * -1 ))"
+		fi
+
+	if [[ "$SEC" -gt 60 ]]
+		then
+			MIN="$(( SEC / 60 ))"
+			SEC="$(( SEC % 60 ))"
+		fi
+	[[ "$SEC" -gt 0 ]] && RET="${SEC}s${RET}"
+
+	if [[ "$MIN" -gt 60 ]]
+		then
+			HOUR="$(( MIN / 60 ))"
+			MIN="$(( MIN % 60 ))"
+		fi
+	[[ "$MIN" -gt 0 ]] && RET="${MIN}m${RET}"
+
+	if [[ "$HOUR" -gt 24 ]]
+		then
+			DAY="$(( HOUR / 24 ))"
+			HOUR="$(( HOUR % 24 ))"
+		fi
+	[[ "$HOUR" -gt 0 ]] && RET="${HOUR}h${RET}"
+
+	if [[ "$DAY" -gt 7 ]]
+		then
+			WEEK="$(( DAY / 7 ))"
+			DAY="$(( DAY % 7 ))"
+		fi
+	[[ "$DAY" -gt 0 ]] && RET="${DAY}d ${RET}"
+	[[ "$WEEK" -gt 0 ]] && RET="${WEEK}w${RET}"
+
+
+	echo "$SGN$RET"
+}
+
 CONV_2DATETIMESTR() {
 	DEBUG "$@"
 	ASSERT_NOTEMPTY "$1" || FAIL 99
